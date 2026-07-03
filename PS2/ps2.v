7080c5b866c5d8760fef42ffb1e428192a425be6
Yang <546883777@qq.com>
2026-07-04 00:20:10 +0800

    fix: 修改了状态机语义更明确，通过data path

--- PS2/ps2.v
+++ PS2/ps2.v
@@ -2,52 +2,53 @@ module top_module (
     input clk,
     input reset,
     input [7:0] in,
+    output reg [23:0] out_bytes,
     output  reg done
 );
     localparam 
-        IDLE = 0,
-        BYTE0 = 1,
-        BYTE1 = 2,
-        BYTE2 = 3;
+        BYTE0 = 0,
+        BYTE1 = 1,
+        BYTE2 = 2;
 
     reg [7:0] state_reg;
     reg [7:0] state_next;
+    reg [23:0] out_bytes_next;
+    reg done_next;
 
     always @(*) begin
-        state_next = IDLE;
-        done = 0;
+        state_next = BYTE0;
+        done_next = 0;
+        out_bytes_next = out_bytes;
         case (state_reg)
-            IDLE: begin
+            BYTE0: begin
+                out_bytes_next[23:16] = in; 
                 if(in[3] == 1) begin
-                    state_next = BYTE0;
+                    state_next = BYTE1;
                 end
             end
-            BYTE0: begin
-                state_next = BYTE1;
-            end
             BYTE1: begin
+                out_bytes_next[15:8] = in;
                 state_next = BYTE2;
             end
             BYTE2: begin
-                done = 1;
+                out_bytes_next[7:0] = in;
+                done_next = 1;
+                state_next = BYTE0;
 
-                if(in[3] == 1) begin
-                    state_next = BYTE0;
-                end
-                else begin
-                    state_next = IDLE;
-                end
             end
-            default: state_next = IDLE;
+            default: state_next = BYTE0;
         endcase
     end
 
     always @(posedge clk,posedge reset) begin
         if(reset) begin
-            state_reg <= IDLE;
+            state_reg <= BYTE0;
+            out_bytes <= 0;
         end
         else begin
             state_reg <= state_next;
+            out_bytes <= out_bytes_next;
+            done <= done_next;
         end
     end
 
